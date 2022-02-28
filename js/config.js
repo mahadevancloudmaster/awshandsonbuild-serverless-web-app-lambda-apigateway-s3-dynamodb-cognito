17d3c2fb3d7ae7b25bd202f1d05138f55d0cf286
mahadevancloudmaster <89058805+mahadevancloudmaster@users.noreply.github.com>
2022-02-28 15:12:50 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_x27V6kR0z', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4b2evqqqp754dohd9nhch8p9jq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'ap-south-1_JCWzCHbQ5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7cvacg1ubqkvbvlpnld2n3si9d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://qi0w781623.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://qi0w781623.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
