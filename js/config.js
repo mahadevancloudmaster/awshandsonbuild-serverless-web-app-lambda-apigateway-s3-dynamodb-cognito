8257578ea4059c1622debd0aa3b84d4026202981
mahadevancloudmaster <89058805+mahadevancloudmaster@users.noreply.github.com>
2022-02-28 15:16:40 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_JCWzCHbQ5', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7cvacg1ubqkvbvlpnld2n3si9d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2a3nvuda0d38llr6cldnqrkhdl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
