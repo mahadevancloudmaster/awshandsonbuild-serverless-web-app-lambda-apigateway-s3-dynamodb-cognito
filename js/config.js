70869cb37e9cbecfb57f3afbdd878c8fa78cdabe
mahadevancloudmaster <89058805+mahadevancloudmaster@users.noreply.github.com>
2022-02-28 15:50:19 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://qi0w781623.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://ho9kzi374a.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
